d26e048b7afec2b640400e17126a786c0a4757c9
manhlab <manhtranduc1111@gmail.com>
2021-12-25 11:49:17 +0300

    update app.py with fastapi and asynch

--- app.py
+++ app.py
@@ -1,30 +1,26 @@
-# -*- encoding: utf-8 -*-
-"""
-Copyright (c) 2019 - present AppSeed.us
-"""
+from importlib import import_module
+from fastapi import FastAPI, Request
+from fastapi.responses import HTMLResponse
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
+import uvicorn
+from dotenv import load_dotenv, dotenv_values
+import os
+load_dotenv() 
 
-from sys import exit
-from decouple import config
+def create_app():
+    app = FastAPI(debug=True)
+    app.mount("/static", StaticFiles(directory="apps/static"), name="static")
+    templates = Jinja2Templates(directory="apps/templates")
+    @app.get("/", response_class=HTMLResponse)
+    async def get_webpage(request: Request):
+        return templates.TemplateResponse(name="index.html", context={"request": request})
+    @app.get("/dashboard", response_class=HTMLResponse)
+    async def route_template(request : Request):
+        return templates.TemplateResponse(name="dashboard.html", context={"request": request})
 
-from apps.config import config_dict
-from apps import create_app
+    return app
+app = create_app()
 
-# WARNING: Don't run with debug turned on in production!
-DEBUG = config("DEBUG", default=True, cast=bool)
-
-# The configuration
-get_config_mode = "Debug" if DEBUG else "Production"
-
-try:
-    # Load the configuration using the default values
-    app_config = config_dict[get_config_mode.capitalize()]
-except KeyError:
-    exit("Error: Invalid <config_mode>. Expected values [Debug, Production] ")
-
-app = create_app(app_config)
-if DEBUG:
-    app.logger.info("DEBUG       = " + str(DEBUG))
-    app.logger.info("Environment = " + get_config_mode)
-
-if __name__ == "__main__":
-    app.run()
+if __name__ == '__main__':
+    uvicorn.run("app:app", host=os.getenv("HOST_ADDRESS"), port=int(os.getenv("PORT")))
