19dee6716b16c6162ededdc95c7f9195cc2c700e
manhlab <manhtranduc1111@gmail.com>
2022-04-29 19:38:14 +0300

    upate api

--- app.py
+++ app.py
@@ -3,40 +3,39 @@ from fastapi import FastAPI, Request
 from fastapi.responses import HTMLResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
+from pydantic import BaseModel
+
 import uvicorn
 from dotenv import load_dotenv
 import os
 
 import torch
 from mongo_connect import mongo_config, import2mongo, export2mongo
-
+from prediction import inference
 load_dotenv()
 
 app = FastAPI(debug=True)
 app.mount("/static", StaticFiles(directory="apps/static"), name="static")
 templates = Jinja2Templates(directory="apps/templates")
+class Item(BaseModel):
+          language = "english"
 
-
+class Checkcode(BaseModel):
+    hash: str
 @app.get("/", response_class=HTMLResponse)
 async def get_home(request: Request):
     return templates.TemplateResponse(name="index.html", context={"request": request})
 
-
 @app.get("/dashboard", response_class=HTMLResponse)
-async def get_dashboard(request: Request):
+async def get_dashboard(request: Request, hash: str):
+    info = import2mongo(hash)
     return templates.TemplateResponse(
-        name="dashboard.html", context={"request": request}
+        name="dashboard.html", context={"request": info}
     )
 
-    return app
-
-@app.get("/predict", response_class=HTMLResponse)
+@app.get("/predict", response_class=Checkcode)
 async def get_dashboard(request: Request):
-    return templates.TemplateResponse(
-        name="dashboard.html", context={"request": request}
-    )
-
-    return app
+    return inference(request)
 
 if __name__ == "__main__":
     uvicorn.run("app:app", host=os.getenv("HOST_ADDRESS"), port=int(os.getenv("PORT")))
